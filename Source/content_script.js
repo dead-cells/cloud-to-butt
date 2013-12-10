615f09c1adf72ff05c09a3060925a6d91852b5f9
detritus <deadcells@riseup.net>
2013-12-10 16:20:39 +0000

    Modified the replacement string
    modified replacement string to reveal the true nature of the cloud

--- Source/content_script.js
+++ Source/content_script.js
@@ -31,10 +31,10 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bThe Cloud\b/g, "The Spectacle");
+	v = v.replace(/\bThe cloud\b/g, "The spectacle");
+	v = v.replace(/\bthe Cloud\b/g, "the Spectacle");
+	v = v.replace(/\bthe cloud\b/g, "the spectacle");
 	
 	textNode.nodeValue = v;
 }
